a3d8ae0501e77f18e1dfe1d6bb9eec64f614e1bf
BarracudaPff <mrm.kikll@gmail.com>
2022-02-03 18:30:34 +0300

    add inner lambdas

---
+++ src/main/kotlin/ru/otus/otuskotlin/dsl/InnerLambdas.kt
@@ -0,0 +1,42 @@
+package ru.otus.okb.first.ru.otus.otuskotlin.dsl
+
+// Inner lambdas
+// All in once - https://pl.kotl.in/3GcAl1Sj2
+// ALL ARE SAME SAMPLES, BUT DIFFERENT CODE
+////////////////////////////////////////////////////////////////////////////////
+// Shuffle string
+
+fun shuffleSimple(value: String): String {
+    val indexes = (0 until value.length).shuffled()
+    val sb = StringBuilder()
+    indexes.forEach { index ->
+        sb.append(value[index])
+    }
+    return sb.toString()
+}
+
+
+fun String.shuffleFoo(): String {
+    val indexes = (0 until this.length).shuffled()
+    val sb = StringBuilder()
+    indexes.forEach { index ->
+        sb.append(this[index])
+    }
+    return sb.toString()
+}
+
+val shuffle: String.() -> String = {
+    val indexes = (0 until this.length).shuffled()
+    val sb = StringBuilder()
+    indexes.forEach { index ->
+        sb.append(this[index])
+    }
+    sb.toString()
+}
+
+fun main() {
+    // Inner lambdas
+    println(shuffleSimple("My simple line"))
+    println("My simple line".shuffleFoo())
+    println("My simple line".shuffle())
+}
